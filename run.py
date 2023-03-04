0c69db99eb1ef47e2d22d20a991c0232706dac7e
pengxiaoo <alexpengxiao@gmail.com>
2023-03-04 16:27:35 +0800

    update midigator

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read(os.path.abspath(os.path.join(".ini")))
+mongo_uri = config['PROD']['DB_URI']
+app = create_app(mongo_uri)
+app.config['DEBUG'] = True
+app.config['MONGO_URI'] = mongo_uri
 
 if __name__ == "__main__":
-    mongo_uri = config['PROD']['DB_URI']
-    app = create_app(mongo_uri)
-    app.config['DEBUG'] = True
-    app.config['MONGO_URI'] = mongo_uri
     app.run(host="0.0.0.0")
