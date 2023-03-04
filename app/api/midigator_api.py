10ddadaf92d72876c7d1b9a8ec0cb228439f112f
pengxiaoo <alexpengxiao@gmail.com>
2023-03-04 17:37:17 +0800

    update midigator

--- app/api/midigator_api.py
+++ app/api/midigator_api.py
@@ -13,19 +13,20 @@ def get_col(col_name, db_name='midigator'):
     return mongo.cx[db_name][col_name]
 
 
+@midigator_api_v1.route('/', methods=['GET'])
+def api_hello():
+    response = {
+        "greetings": "you have setup flask successfully! you need to install mongodb if you haven't done so!",
+        "page": 0,
+    }
+    return make_response(jsonify(response), 200)
+
+
 @midigator_api_v1.route('/', methods=['POST'])
 def api_registration_new():
     body = request.get_json()
     print(body)
     data = {'ack': 'successfully received registration-new-event!'}
-    return make_response(jsonify(data), 200)
-
-
-@midigator_api_v1.route('/order-validation-new-event', methods=['POST'])
-def api_order_validation_new():
-    body = request.get_json()
-    print(body)
-    data = {'ack': 'successfully received order-validation-new-event!'}
     return make_response(jsonify(data), 200)
 
 
@@ -37,13 +38,44 @@ def api_chargeback_new():
     return make_response(jsonify(data), 200)
 
 
-@midigator_api_v1.route('/', methods=['GET'])
-def api_hello():
-    response = {
-        "greetings": "you have setup flask successfully! you need to install mongodb if you haven't done so!",
-        "page": 0,
-    }
-    return jsonify(response)
+@midigator_api_v1.route('/chargeback-match-event', methods=['POST'])
+def api_chargeback_match():
+    body = request.get_json()
+    print(body)
+    data = {'ack': 'successfully received chargeback-match-event!'}
+    return make_response(jsonify(data), 200)
+
+
+@midigator_api_v1.route('/order-validation-new-event', methods=['POST'])
+def api_order_validation_new():
+    body = request.get_json()
+    print(body)
+    data = {'ack': 'successfully received order-validation-new-event!'}
+    return make_response(jsonify(data), 200)
+
+
+@midigator_api_v1.route('/order-validation-match-event', methods=['POST'])
+def api_order_validation_match():
+    body = request.get_json()
+    print(body)
+    data = {'ack': 'successfully received order-validation-match-event!'}
+    return make_response(jsonify(data), 200)
+
+
+@midigator_api_v1.route('/prevention-new-event', methods=['POST'])
+def api_prevention_new():
+    body = request.get_json()
+    print(body)
+    data = {'ack': 'successfully received prevention-new-event!'}
+    return make_response(jsonify(data), 200)
+
+
+@midigator_api_v1.route('/prevention-match-event', methods=['POST'])
+def api_prevention_match():
+    body = request.get_json()
+    print(body)
+    data = {'ack': 'successfully received prevention-match-event!'}
+    return make_response(jsonify(data), 200)
 
 
 @midigator_api_v1.route('/order', methods=['GET'])
--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 from app.factory import create_app
-import os
-import configparser
+import settings
 
-config = configparser.ConfigParser()
-# config.read(os.path.abspath(os.path.join(".ini")))
-# mongo_uri = config['PROD']['DB_URI']
-mongo_uri = 'mongodb://localhost:27017/'
+mongo_uri = settings.DB_URI
 app = create_app(mongo_uri)
 app.config['DEBUG'] = True
 app.config['MONGO_URI'] = mongo_uri
