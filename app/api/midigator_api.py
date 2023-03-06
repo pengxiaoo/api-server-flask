013f960e02320379984b59e9fb8d6f1a53830dcf
pengxiaoo <alexpengxiao@gmail.com>
2023-03-06 13:12:16 +0800

    update logging

--- app/api/midigator_api.py
+++ app/api/midigator_api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response
 from flask_cors import CORS
-from app.api.utils import get_col, auth_required
+from app.api.utils import get_col, auth_required, log
 
 midigator_api_v1 = Blueprint(
     'midigator_api_v1', 'midigator_api_v1', url_prefix='/api/v1/midigator')
@@ -10,93 +10,101 @@ CORS(midigator_api_v1)
 @midigator_api_v1.route('/', methods=['GET'])
 @auth_required
 def api_hello():
-    response = {
+    request_json = get_request_json(request)
+    response_body = {
         "greetings": "you have setup flask successfully! you need to install mongodb if you haven't done so!",
         "page": 0,
     }
-    return make_response(jsonify(response), 200)
+    return create_response(request.path, response_body, 200)
 
 
 @midigator_api_v1.route('/', methods=['POST'])
 @auth_required
 def api_registration_new():
-    body = request.get_json()
-    print(body)
-    data = {'ack': 'successfully received registration-new-event!'}
-    return make_response(jsonify(data), 200)
+    request_json = get_request_json(request)
+    response_body = {'ack': f'successfully received {request.path}!'}
+    return create_response(request.path, response_body, 200)
 
 
 @midigator_api_v1.route('/chargeback-new-event', methods=['POST'])
 @auth_required
 def api_chargeback_new():
-    body = request.get_json()
-    print(body)
-    data = {'ack': 'successfully received chargeback-new-event!'}
-    return make_response(jsonify(data), 200)
+    request_json = get_request_json(request)
+    response_body = {'ack': f'successfully received {request.path}!'}
+    return create_response(request.path, response_body, 200)
 
 
 @midigator_api_v1.route('/chargeback-match-event', methods=['POST'])
 @auth_required
 def api_chargeback_match():
-    body = request.get_json()
-    print(body)
-    data = {'ack': 'successfully received chargeback-match-event!'}
-    return make_response(jsonify(data), 200)
+    request_json = get_request_json(request)
+    response_body = {'ack': f'successfully received {request.path}!'}
+    return create_response(request.path, response_body, 200)
 
 
 @midigator_api_v1.route('/order-validation-new-event', methods=['POST'])
 @auth_required
 def api_order_validation_new():
-    body = request.get_json()
-    print(body)
-    data = {'ack': 'successfully received order-validation-new-event!'}
-    return make_response(jsonify(data), 200)
+    request_json = get_request_json(request)
+    response_body = {'ack': f'successfully received {request.path}!'}
+    return create_response(request.path, response_body, 200)
 
 
 @midigator_api_v1.route('/order-validation-match-event', methods=['POST'])
 @auth_required
 def api_order_validation_match():
-    body = request.get_json()
-    print(body)
-    data = {'ack': 'successfully received order-validation-match-event!'}
-    return make_response(jsonify(data), 200)
+    request_json = get_request_json(request)
+    response_body = {'ack': f'successfully received {request.path}!'}
+    return create_response(request.path, response_body, 200)
 
 
 @midigator_api_v1.route('/prevention-new-event', methods=['POST'])
 @auth_required
 def api_prevention_new():
-    body = request.get_json()
-    print(body)
-    data = {'ack': 'successfully received prevention-new-event!'}
-    return make_response(jsonify(data), 200)
+    request_json = get_request_json(request)
+    response_body = {'ack': f'successfully received {request.path}!'}
+    return create_response(request.path, response_body, 200)
 
 
 @midigator_api_v1.route('/prevention-match-event', methods=['POST'])
 @auth_required
 def api_prevention_match():
-    body = request.get_json()
-    print(body)
-    data = {'ack': 'successfully received prevention-match-event!'}
-    return make_response(jsonify(data), 200)
+    request_json = get_request_json(request)
+    response_body = {'ack': f'successfully received {request.path}!'}
+    return create_response(request.path, response_body, 200)
 
 
 @midigator_api_v1.route('/order', methods=['GET'])
 @auth_required
 def api_get_orders():
+    request_json = get_request_json(request)
     order = get_col('orders', 'sticky').find_one({})
-    response = {
+    response_body = {
         "order": dict(order),
         "page": 0,
     }
-    return jsonify(response)
+    return create_response(request.path, response_body, 200)
 
 
 @midigator_api_v1.route('/user', methods=['GET'])
 @auth_required
 def api_get_user():
+    request_json = get_request_json(request)
     user = get_col('EmailUnsubscriber', 'user').find_one({})
-    response = {
+    response_body = {
         "user": dict(user),
         "page": 0,
     }
-    return jsonify(response)
+    return create_response(request.path, response_body, 200)
+
+
+def get_request_json(req):
+    request_json = req.get_json()
+    log({f"received request in {req.path}": request_json})
+    return request_json
+
+
+def create_response(path, response_body, status_code=200):
+    response = make_response(jsonify(response_body), status_code)
+    log({f"send response in {path} with code {status_code}": response.json})
+    return response
--- app/api/utils.py
+++ app/api/utils.py
@@ -2,6 +2,19 @@ from flask import request, make_response
 from app.database import mongo
 from functools import wraps
 import os
+import logging
+
+logging.basicConfig(filename='record.log',
+                    level=logging.DEBUG,
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
+
+def log(text, level='WARNING'):
+    if level == 'WARNING':
+        logging.warning(text)
+    elif level == 'ERROR':
+        logging.error(text)
+
 
 (username, password) = (os.environ['MIDIGATOR_SANDBOX_USERNAME'], os.environ['MIDIGATOR_SANDBOX_PASSWORD'])
 
@@ -12,16 +25,12 @@ def auth_required(f):
         auth = request.authorization
         if auth and auth.username == username and auth.password == password:
             return f(*args, **kwargs)
-        return make_response('basic auth failed, check your username and password!', 401)
+        response = make_response('basic auth failed, check your username and password!', 401)
+        log({f"send response in {request.path} with code {response.status_code}": response.data})
+        return response
 
     return decorated
 
 
 def get_col(col_name, db_name='midigator'):
     return mongo.cx[db_name][col_name]
-
-
-def expect(input, expectedType, field):
-    if isinstance(input, expectedType):
-        return input
-    raise AssertionError("Invalid input for type", field)
